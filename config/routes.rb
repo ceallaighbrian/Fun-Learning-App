a42bca76924cbafc82aa44dc6d71778b1a31d4a6
Brian Kelly <ceallaighbrian@gmail.com>
2015-02-06 23:29:34 +0000

    User sign up

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
-  resources :lesson
+  resources :users, :lesson
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
